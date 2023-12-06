416cbc77c5652e61881bdd13c287f85e9e0522b7
Jared Alexander Trejo Larios <jared.trejo@axity.com>
2023-12-06 13:33:07 -0600

    Add: Subiendo proyecto ✅

---
+++ src/webparts/linkCards/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ILinkCardsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'LinkCardsWebPartStrings' {
+  const strings: ILinkCardsWebPartStrings;
+  export = strings;
+}
